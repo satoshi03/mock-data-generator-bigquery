2b1584fe5efba5ec3c9c00ae02728c218c8b3e90
Satoshi Innami <innamisatoshi@gmail.com>
2016-12-11 15:40:39 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Dummy data generator for BigQuery
+
+Generate dummy/mock data for BigQuery using json schema file.
+
+Generated dummy data file can be loaded to BigQuery.
+
+## How to use
+
+Clone this repository.
+
+```
+git clone git@github.com:satoshi03/mock-data-generator-bigquery.git
+```
+
+Run generator.
+
+```
+python dummy_bq.py <table_schema.json>
+```
+
+Then, dummy data file named "output.json" is created.
+
+```
+cat output.json
+```
+
+## Options
+
+|option|description|default|
+|:---|:---|:---|
+|-o|output file name|output.json|
+|-l|number of lines(rows) of dummy file|1000|
+
+## Examples
+
+examples/flat.json
+
+```json
+[
+    {
+        "name": "hogehoge",
+        "type": "INTEGER",
+        "mode": "REQUIRED"
+    },
+    {
+        "name": "pokopoko",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },
+    {
+        "name": "created_at",
+        "type": "TIMESTAMP",
+        "mode": "REQUIRED"
+    }
+]
+
+```
+
+Generate 10 lines dummy data to output.json
+
+```
+python dummy_bq.py examples/flat.json -l 10
+```
+
+```json
+{"hogehoge": 9997, "created_at": "2016-12-10 18:13:19"}
+{"hogehoge": 2531, "created_at": "2016-12-10 23:53:18"}
+{"hogehoge": 4029, "created_at": "2016-12-11 13:53:50"}
+{"hogehoge": 1084, "created_at": "2016-12-11 14:41:07", "pokopoko": "5G4QCT4HZJ48"}
+{"hogehoge": 3386, "created_at": "2016-12-11 09:16:28", "pokopoko": "9W37EJ1QC0BY"}
+{"hogehoge": 8680, "created_at": "2016-12-11 12:52:20", "pokopoko": "KLP2XNJ0T8ZU"}
+{"hogehoge": 4594, "created_at": "2016-12-10 20:13:28"}
+{"hogehoge": 5584, "created_at": "2016-12-11 06:08:55"}
+{"hogehoge": 6726, "created_at": "2016-12-11 09:05:07"}
+{"hogehoge": 754, "created_at": "2016-12-10 22:48:56"}
+```
+
+Above data can be uploaded to BigQuery using console or cli.
---
+++ dummy_bq.py
@@ -0,0 +1,95 @@
+#!/bin/python
+
+import argparse
+import json
+import random
+import string
+from datetime import datetime as dt
+from datetime import timedelta
+
+
+parser = argparse.ArgumentParser(description="generate mock data for BigQuery.")
+parser.add_argument('input', metavar='FILE', help='input schema file.')
+parser.add_argument('-l', metavar='-l', type=int, default=1000, help='output mock data raws. (default: 1000)')
+parser.add_argument('-o', metavar='-o', default='output.json', help='output mock data file. (default: output.json)')
+
+TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+DUMMY_RAND_INT_MIN = 1
+DUMMY_RAND_INT_MAX = 10000
+
+DUMMY_RAND_FLOAD_MIN = 1
+DUMMY_RAND_FLOAT_MIN = 10000
+
+DUMMY_RAND_TIMESTAMP_MIN = dt.now() - timedelta(1)
+DUMMY_RAND_TIMESTAMP_MAX = dt.now()
+
+MAX_REPEAT_NUM = 5
+
+def random_str(size=12, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def random_timestamp(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = random.randrange(int_delta)
+    return (start + timedelta(seconds=random_second)).strftime(TIMESTAMP_FORMAT)
+
+
+def mockdata(t):
+    if t == 'STRING':
+        return random_str()
+    elif t == 'INTEGER':
+        return random.randint(DUMMY_RAND_INT_MIN, DUMMY_RAND_INT_MAX)
+    elif t == 'FLOAT':
+        return random.randint(DUMMY_RAND_FLOAD_MIN, DUMMY_RAND_INT_MAX - 1) + random.random()
+    elif  t == 'TIMESTAMP':
+        return random_timestamp(DUMMY_RAND_TIMESTAMP_MIN, DUMMY_RAND_TIMESTAMP_MAX)
+    elif t == 'BOOLEAN':
+        return bool(random.getrandbits(1))
+
+
+def generate(schema):
+    dic = {}
+    for column in schema:
+        if column['MODE'] == 'REPEAT':
+            if column['TYPE'] == 'RECORD':
+                dic[column['NAME']] = [generate(column['FIELDS']) for i in range(0, random.randrange(MAX_REPEAT_NUM) + 1)]
+            else:
+                dic[column['NAME']] = [mockdata(column['TYPE']) for i in range(0, random.randrange(MAX_REPEAT_NUM) + 1)]
+        else:
+            if column['MODE'] == 'NULLABLE':
+                if random.randint(0, 1):
+                    continue
+
+            if column['TYPE'] == 'RECORD':
+                dic[column['NAME']] = generate(column['FIELDS'])
+            else:
+                dic[column['NAME']] = mockdata(column['TYPE'])
+    return dic
+
+
+def to_upper(schema):
+    l = []
+    for column in schema:
+        d = {}
+        for k, v in column.items():
+            if isinstance(k, str):
+                k = k.upper()
+            if isinstance(v, str):
+                if k != 'NAME':
+                    v = v.upper()
+            if isinstance(v, list):
+                v = to_upper(v)
+            d[k] = v
+        l.append(d)
+    return l
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    with open(args.input, 'r') as ijf, open(args.o, 'w') as ojf:
+        schema = to_upper(json.load(ijf))
+        for l in range(0, args.l):
+            ojf.write(json.dumps(generate(schema)) + '\n')
